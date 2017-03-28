e5ef18b5f7a1450ab8ef8b77dec6cf088b0abf36
Pitt009 <gmartinez28837@gmail.com>
2017-03-28 10:54:36 -0600

    intento de trello1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
+function agregarPendiente() {
+  var input = document.getElementById('texto').value;
+  document.getElementById("lista").innerHTML = input;
+  input.setAttribute("lista");
+   document.body.appendChild(input);
 
+   var elemento1 = document.createElement("li");
+   var tex = document.createTextNode("input");
+   elemento1.appendChild(tex);
+   document.getElementById('lista').appendChild(elemento1);
+}
