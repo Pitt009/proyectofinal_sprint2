eff9b3762b5691457daaada107012eb607eab040
Pitt009 <gmartinez28837@gmail.com>
2017-03-28 13:15:50 -0600

    intento sin solucion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
-function agregarPendiente() {
-  var input = document.getElementById('texto').value;
-  document.getElementById("lista").innerHTML = input;
-  input.setAttribute("lista");
-   document.body.appendChild(input);
+/*function agregarPendiente() {
+  var inputText = document.getElementById('texto').value;
+  document.getElementById("lista").innerHTML = inputText;
+  inputText.setAttribute("lista");
+   document.body.appendChild(inputText);
 
    var elemento1 = document.createElement("li");
-   var tex = document.createTextNode("input");
+   var tex = document.createTextNode("inputText");
    elemento1.appendChild(tex);
    document.getElementById('lista').appendChild(elemento1);
+
+}
+*/
+
+function agregarLista(){
+ var crearLista = document.getElementById('crearLista').value
+ console.log(crearLista);
+ var ul = document.getElementById("ul");
+ var li = document.createElement("li");
+ li.appendChild(document.createTextNode("crearLista"));
+  ul.appendChild(li);
+
+}
+function agregarPendiente() {
+  var inputText = document.getElementById('texto').value;
+  document.getElementById('tituloLista').innerHTML = inputText;
 }
